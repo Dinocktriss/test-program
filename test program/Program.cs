3cc5632d1aba40a0b3a766a27a177dfd09b1abf1
Dinocktriss <hobu4oku@gmail.com>
2024-11-28 11:59:56 +0900

    1.0.0
    тестовая версия без возможности взаимодействия с пользователем

---
+++ test program/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using InspectorLib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FunctionInsp insp = new FunctionInsp();
+
+        // Тесты методов
+        Console.WriteLine(insp.GetInspector()); // Васильев Василий Иванович
+        Console.WriteLine(insp.GetCarInspection()); // Автоинспекция г. Чита
+
+        Console.WriteLine(insp.SetInspector("Иванов И.И.")); // True
+        Console.WriteLine(insp.GetInspector()); // Иванов И.И.
+
+        Console.WriteLine(insp.GenerateNumber(1234, 'A', 75)); // A1234_75
+
+        var workers = insp.GetWorker();
+        workers.ForEach(worker => Console.WriteLine(worker));
+
+        insp.AddWorker("Сидоров С.С.");
+        workers = insp.GetWorker();
+        workers.ForEach(worker => Console.WriteLine(worker));
+    }
+}
